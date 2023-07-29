e5ef7814d7ea5e2e7db6092c2b9030c86aa78812
shiprahul <101277713+shiprahul@users.noreply.github.com>
2023-07-28 20:32:37 -0400

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,17 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1#increasing the value
+        else:
+            right = mid - 1#decreasing the value
+
+    return -1
+  '''
+  In this example we add the binary serach algo and we return -1 if we don't find an target'''
